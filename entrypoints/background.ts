c60e37fa82519b00a6cff3f169af0a88dcd048fa
nito-008 <100199931+nito-008@users.noreply.github.com>
2026-07-06 17:51:15 +0900

    feat: add downloads.onCreated event listener

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -4,12 +4,16 @@ let latestCourseName = "";
 async function getCourseName(tabId: number): Promise<string> {
   const result = await browser.scripting.executeScript({
     target: { tabId },
-    func: () => document.querySelector('#coursename')?.innerHTML,
+    func: () => document.querySelector("#coursename")?.textContent,
   });
   return result[0]?.result ?? "";
 }
 
-async function onTabUpdate(tabId: number, changeInfo: Browser.tabs.OnUpdatedInfo, tab: Browser.tabs.Tab) {
+async function onTabUpdate(
+  tabId: number,
+  changeInfo: Browser.tabs.OnUpdatedInfo,
+  tab: Browser.tabs.Tab,
+) {
   if (changeInfo.status !== "complete") return;
 
   const courseName = await getCourseName(tabId);
@@ -19,7 +23,39 @@ async function onTabUpdate(tabId: number, changeInfo: Browser.tabs.OnUpdatedInfo
   console.log("Course name updated: ", courseName);
 }
 
+function getBaseName(filePath: string): string {
+  return filePath.split(/[\\/]/).pop() ?? "";
+}
+
+function getDownloadPath(fileName: string): string {
+  return [BASE_DIR_NAME, latestCourseName, getBaseName(fileName)].join("/");
+}
+
+function onDownloadCreated(downloadItem: Browser.downloads.DownloadItem) {
+  if (downloadItem.byExtensionId) return;
+
+  browser.downloads
+    .cancel(downloadItem.id)
+    .then(() => {
+      return browser.downloads.erase({ id: downloadItem.id });
+    })
+    .then(() => {
+      const filePath = getDownloadPath(downloadItem.filename);
+      console.log("Download file: ", filePath);
+
+      return browser.downloads.download({
+        url: downloadItem.url,
+        filename: filePath,
+        saveAs: false,
+      });
+    })
+    .catch((error) => {
+      console.error("Failed to download file: ", error);
+    });
+}
+
 export default defineBackground(() => {
   browser.tabs.onUpdated.addListener(onTabUpdate);
-  console.log('Hello background!', { id: browser.runtime.id });
+  browser.downloads.onCreated.addListener(onDownloadCreated);
+  console.log("Hello background!", { id: browser.runtime.id });
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifestVersion: 3,
   manifest: {
-    permissions: ['tabs', 'scripting'],
-    host_permissions: ['<all_urls>'],
+    permissions: ["tabs", "scripting", "downloads"],
+    host_permissions: ["<all_urls>"],
     browser_specific_settings: {
       gecko: {
-        id: 'manaba-download-router-firefox@nito-008.github.com',
+        id: "manaba-download-router-firefox@nito-008.github.com",
         data_collection_permissions: {
-          required: ['none'],
+          required: ["none"],
         },
       },
     },
